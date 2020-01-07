499a759220fd81a5cc67274fd1cd2c5cf15e4607
kishan singh <kishan.singh@mountblue.io>
2020-01-07 17:46:51 +0530

    Added ternary operator to handle JSX.

--- src/App.js
+++ src/App.js
@@ -9,7 +9,8 @@ class App extends Component {
       { name:"kanhaiya", age: 23 },
       { name:"Lucky", age: 22 },
       { name:"Sweta", age: 20 }
-    ]
+    ],
+    showPerson:false
   }
 
    onClickHandler = (newName) => {
@@ -24,7 +25,7 @@ class App extends Component {
      )
   }
 
-  nameChangeHandler = (event) => {
+  nameChangeHandler = (event) => { 
     this.setState({
        persons:[
          { name: "kishan", age: 25 },
@@ -36,31 +37,39 @@ class App extends Component {
     )
  }
 
+ toggelPersonHandler = () => {
+ const doesShow = this.state.showPerson;
+ this.setState({showPerson: !doesShow});
+ }
+
   render(){
   return (
     <div className="App">
         <h1>I am React App </h1>
-        <button onClick = {this.onClickHandler.bind(this, "Kishan Singh")}>Click me</button>
+        <button onClick = { this.toggelPersonHandler }>Click me</button>
+        { this.state.showPerson === true ?
+              <div>
+                 <Person name = {this.state.persons[0].name}
+                  age = {this.state.persons[0].age}/>
 
-        <Person name = {this.state.persons[0].name}
-         age = {this.state.persons[0].age}/>
+                 <Person name = {this.state.persons[1].name}
+                  age = {this.state.persons[1].age}
+                  nameChange = {this.nameChangeHandler}/>
 
-        <Person name = {this.state.persons[1].name}
-         age = {this.state.persons[1].age}
-         nameChange = {this.nameChangeHandler}/>
+                 <Person name = {this.state.persons[2].name}
+                  age = {this.state.persons[2].age}/>
 
-        <Person name = {this.state.persons[2].name}
-         age = {this.state.persons[2].age}/>
-
-        <Person name = {this.state.persons[3].name}
-                age = {this.state.persons[3].age}
-                click = {this.onClickHandler.bind(this, "Krish")}>
-                  <h2>@author</h2> 
-        </Person>
-    </div>
+                 <Person name = {this.state.persons[3].name}
+                         age = {this.state.persons[3].age}
+                         click = {this.onClickHandler.bind(this, "Krish")} >
+                           <h2>@author</h2> 
+                 </Person>
+              </div> : null
+          } 
+    </div>     
   );
 }
 }
 
-export default App;
+export default  App;
  
